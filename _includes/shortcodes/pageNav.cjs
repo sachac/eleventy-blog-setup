ee80af11850de4fc76d16e1d0b34eb3506a0ac46
Sacha Chua <sacha@sachachua.com>
2024-10-31 14:07:18 -0400

    before removing ellipsis; tinkering with page nav

--- _includes/shortcodes/pageNav.cjs
+++ _includes/shortcodes/pageNav.cjs
@@ -1,6 +1,6 @@
 module.exports = (eleventyConfig) =>
 
-eleventyConfig.addShortcode('pageNav', (data) => {
+eleventyConfig.addShortcode('pageNav', (data, item) => {
   /* Prev Page 1 Page 2 Page 3 Page 4 Page 5 All Next */
   let prev = data.pagination.pageNumber > 0 
       ? data.pagination.pages[data.pagination.pageNumber - 1] : null;
@@ -17,20 +17,26 @@ eleventyConfig.addShortcode('pageNav', (data) => {
   if (current.type) {
     all = `/blog/${current.type}/${current.key}/all/`;
   }
-  let pages = (data.includePages || data.pagination.includePages) ? data.pagination.pages.map(function(item, index) {
-    if (item.type && !(item.type == current.type && item.key == current.key)) {
-      return null;
-    }
-    let displayPageNum = ((typeof item.page !== 'undefined') ? item.page : index);
-    let currentClass = index == data.pagination.pageNumber ? 'current' : '';
-    return `<li class="${currentClass}"><a href="${data.pagination.hrefs[index]}" ${data.pagination.hrefs[index] ? 'aria-current="page"' : "" }>Page ${displayPageNum + 1}</a></li>`;
-  }).join("") : '';
   // console.log(data.page.url, next && data.pagination.hrefs[data.pagination.pageNumber + 1]);
   let prevNav = prev ? `<li><a href="${data.pagination.hrefs[data.pagination.pageNumber - 1]}">&laquo; Previous page</a></li>`: '';
   let nextNav = next ? `<li><a href="${data.pagination.hrefs[data.pagination.pageNumber + 1]}">Next page &raquo;</a></li>`: '';
+	let allLink = '';
+	if (typeof item?.numPages == 'undefined' || item.numPages > 1) {
+		if (item && item.total) {
+			let more = '';
+			const numPages = 5;
+			const perPage = 7;
+			if (item.total > numPages + perPage) {
+				more = '... ';
+			}
+			allLink += `<li><a href="${all}">${more}All ${item.total} posts</a></li>`;
+		} else {
+			allLink = `<li><a href="${all}">All posts</a></li>`;
+		}
+	}
   let s = `<nav aria-labelledby="pagination" class="page-nav">
     <h2 id="pagination">Page navigation</h2>
-    <ul>${prevNav}${pages}<li><a href="${all}">All</a></li>${nextNav}</ul>
+    <ul>${prevNav}${current}${nextNav}${allLink}</ul>
   </nav>`;
   return s;
 });
