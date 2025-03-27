a5e0294ff040ae10e8d09402031c1789659ddee4
Sacha Chua <sacha@sachachua.com>
2025-03-27 13:03:59 -0400

    sort by date

--- _includes/group.js
+++ _includes/group.js
@@ -9,12 +9,19 @@ export function makeArrayOfObjectEntries(o) {
 
 export function groupByDateFormat(collectionApi, format) {
 	return makeArrayOfObjectEntries(
-    _.groupBy(collectionApi.getFilteredByTag('_posts').map((o) => {
+    _.groupBy(collectionApi.getFilteredByTag('_posts').sort((a, b) => {
+			if (a.date > b.date) return -1;
+			if (a.date < b.date) return 1;
+			return 0;
+		}).map((o) => {
 			return {date: o.date, url: o.url, data: { title: o.data.title,
 																								categories: o.data.categories,
 																								page: { url: o.data.page.url }
 																							}
 						 };
-		}),
-                o => moment(o.date).tz('America/Toronto').format(format)));
+		}), o => moment(o.date).tz('America/Toronto').format(format))).sort((a, b) => {
+			if (a.key < b.key) return 1;
+			if (a.key > b.key) return -1;
+			return 0;
+		});
 };
