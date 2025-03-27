5ce42ccba0fef86cd2bcd39486095166531501a7
Sacha Chua <sacha@sachachua.com>
2025-03-27 13:05:00 -0400

    add post graph

--- content/allPosts.11ty.js
+++ content/allPosts.11ty.js
@@ -7,9 +7,13 @@ export default class AllPosts {
     };
   }
   async render (data) {
-    let list = data.collections._years.reverse();
+    let list = data.collections._years.sort((a, b) => {
+			if (a > b) return -1;
+			if (a < b) return 1;
+			return 0;
+		});
     return `<section><h2>All ${data.collections._posts.length} posts</h2>
-<p>This page lists posts by date. You might also like to explore <a href="/topic">a topic-based outline</a> or <a href="/blog/category">categories</a>.</p>
+<p>This page lists posts by date. You might also like to explore <a href="/topic">a topic-based outline</a>, <a href="/blog/category">categories</a>, or <a href="/blog/on-this-day/">posts written on this day</a>. There's also <a href="/blog/all/graph/">a visualization</a> and a <a href="/blog/all/index.json">JSON file</a>.</p>
 <ul class="inline-list">
 ${await list.reduce(async (prev, year) => {
 return (await prev) +
@@ -19,7 +23,7 @@ return (await prev) +
 ${await list.reduce(async (prev, year) => {
 return (await prev) +
 `<details open><summary><h3 id="year-${year.key}">${year.key}</h3></summary>
-					${await this.archive(year.list.reverse())}</details>`;
+					${await this.archive(year.list)}</details>`;
  }, '')}
 </section>`;
   }
--- content/years.11ty.cjs
+++ content/years.11ty.cjs
@@ -19,8 +19,9 @@ module.exports = class Years {
   async render(data) {
     return `<article>
 ${this.pageNav(data)}
-<h2>${data.title}</h2>
-${await this.archive(data.pagination.items[0].list.reverse(), 'table')}
+<h2>${data.title} (${data.pagination.items[0].list.length} post${data.pagination.items[0].list.length == 1 ? '' : 's'})</h2>
+<div class="post-graph">${this.postGraph(data.pagination.items[0].list)}</div>
+${await this.archive(data.pagination.items[0].list, 'table')}
 </article>
 `;
   }
