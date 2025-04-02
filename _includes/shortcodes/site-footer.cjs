64aac881205adc062c972b8c97e20993b5bd001b
Sacha Chua <sacha@sachachua.com>
2025-04-02 10:36:26 -0400

    add rel tags

--- _includes/shortcodes/site-footer.cjs
+++ _includes/shortcodes/site-footer.cjs
@@ -14,7 +14,7 @@ module.exports = eleventyConfig =>
     <li><a href="/blog/random/#redirect">Random</a></li>
     <li><a href="https://sketches.sachachua.com">Sketches</a></li>
   </ul>
-<p><a href="mailto:sacha@sachachua.com">Email</a> - <a rel="me" href="https://social.sachachua.com/@sacha">Mastodon</a> - <a href="https://github.com/sachac/">Github</a></p>
+<p><a rel="me" href="mailto:sacha@sachachua.com">Email</a> - <a rel="me" href="https://social.sachachua.com/@sacha">Mastodon</a> - <a rel="me" href="https://github.com/sachac/">Github</a></p>
   <p>
     Copyright &#169; 2001-2024 Sacha Chua (<a href="mailto:sacha@sachachua.com">sacha@sachachua.com</a>). Emacs code snippets are generally under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPLv3+</a>; other snippets are dual-licensed under the <a href="https://opensource.org/licenses/MIT">MIT License</a> or GPLv3+ unless otherwise indicated. Please feel free to reuse or share stuff under a <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a> license unless otherwise noted. Thanks for reading!
   </p>
